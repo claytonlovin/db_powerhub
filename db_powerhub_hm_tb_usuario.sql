626024bd6a6ccee0a26a6fb8d3a51c26f899370f
Clayton Silva <57988870+claytonlovin@users.noreply.github.com>
2022-12-28 20:11:52 -0300

    finalizing project

---
+++ db_powerhub_hm_tb_usuario.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_powerhub_hm
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_usuario` (
+  `ID_USUARIO` int NOT NULL AUTO_INCREMENT,
+  `NOME_USUARIO` varchar(100) NOT NULL,
+  `DS_TELEFONE` varchar(15) NOT NULL,
+  `DS_EMAIL` varchar(100) NOT NULL,
+  `DS_LOGIN` varchar(30) NOT NULL,
+  `DS_SENHA` varchar(256) NOT NULL,
+  `FL_ADMINISTRADOR` tinyint(1) DEFAULT (1),
+  `ID_ORGANIZACAO` varchar(256) NOT NULL,
+  `FL_PROPRIETARIO_CONTA` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`ID_USUARIO`),
+  UNIQUE KEY `NOME_USUARIO` (`NOME_USUARIO`),
+  UNIQUE KEY `DS_TELEFONE` (`DS_TELEFONE`),
+  UNIQUE KEY `DS_EMAIL` (`DS_EMAIL`)
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_usuario`
+--
+
+LOCK TABLES `tb_usuario` WRITE;
+/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
+INSERT INTO `tb_usuario` VALUES (101,'Clayton','319987612728','clayton@email.com','admin','admin',1,'98',1),(105,'madalena','28343947392','malena@malena.com.br','malena','admin',0,'98',0);
+/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-28 20:10:44
